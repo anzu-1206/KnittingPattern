614ca415a1fe36dbb25c03e84d2d2d497a08deda
anzu-1206 <alexandros_0201@icloud.com>
2026-02-24 01:41:17 +0900

    マイグレーション実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "makepatterns/index"
+  get "posts/index"
   get "patterns/index"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
