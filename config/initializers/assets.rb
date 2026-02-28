1179da118bd370455d90bb59728f8f2fd403d781
Anzu <alexandros_0201@icloud.com>
2026-02-28 17:57:10 +0900

    [update]新規登録ページのhtml/cssデザイン作成

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -20,3 +20,4 @@ Rails.application.config.assets.precompile += %w( makepatterns_index.css )
 Rails.application.config.assets.precompile += %w( patterns_index.css )
 
 Rails.application.config.assets.precompile += %w( sessions_new.css )
+Rails.application.config.assets.precompile += %w( registrations_new.css )
