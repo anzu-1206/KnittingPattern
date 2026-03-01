84ddd46bd8e55940accc21cd3377372a53f5c4d6
Anzu <alexandros_0201@icloud.com>
2026-03-02 00:20:38 +0900

    [update]ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   get "posts/index"
   get "patterns/index"
   devise_for :users
+
+  devise_scope :user do
+  get 'account_setting', to: 'users/sessions#account_setting', as: :account_setting_user_session
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
