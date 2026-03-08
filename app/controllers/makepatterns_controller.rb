d6874dfc24628def753f4635a3cfd0038d0bae2c
Anzu <alexandros_0201@icloud.com>
2026-03-09 02:14:03 +0900

    [update]コントローラーたちの編集

--- app/controllers/makepatterns_controller.rb
+++ app/controllers/makepatterns_controller.rb
@@ -7,23 +7,4 @@ class MakepatternsController < ApplicationController
 
   def aboutme
   end
-
-  def create
-    @pattern = Pattern.new(pattern_params)
-    
-    if @pattern.save
-      # 保存成功：JS側に成功を伝える
-      render json: { id: @pattern.id, status: 'success' }, status: :created
-    else
-      # 保存失敗：エラーメッセージを返す
-      render json: { errors: @pattern.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
-  private
-
-  def pattern_params
-    # 送られてくるパラメータを許可する
-    params.require(:pattern).permit(:title, :start_type, :data)
-  end
 end
--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -2,4 +2,28 @@ class PatternsController < ApplicationController
   def index
     @patterns = Pattern.all
   end
+
+  class PatternsController < ApplicationController
+  # 保存処理 (Ajaxから呼ばれる)
+  def create
+    @pattern = current_user.patterns.build(pattern_params)
+    if @pattern.save
+      render json: { status: 'success' }
+    else
+      render json: { status: 'error' }, status: 422
+    end
+  end
+
+  # 詳細表示 (保存した編み図を見る)
+  def show
+    @pattern = Pattern.find(params[:id])
+    # @pattern.pattern_data を使ってビューで編み図を復元できる！
+  end
+
+  private
+  def pattern_params
+    # ここで pattern_data や grid_width も許可する
+    params.require(:pattern).permit(:name, :pattern_data, :grid_width, :grid_height, :yarn, :hook, ...)
+  end
+end
 end
