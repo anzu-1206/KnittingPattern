99f2f5fcb109bc6ac069e5c67f328e0a9e8270f6
Anzu <alexandros_0201@icloud.com>
2026-03-15 17:44:49 +0900

    [add]_bookmark.html.erbの作成

--- app/controllers/bookmark_controller.rb
+++ app/controllers/bookmark_controller.rb
@@ -1,9 +1,26 @@
 class BookmarkController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
+    @post = Post.find(params[:post_id])
+    current_user.bookmarks.create(post: @post)
+    
+    render turbo_stream: turbo_stream.replace(
+      "bookmark_button_#{@post.id}",
+      partial: "bookmarks/bookmark",
+      locals: { post: @post }
+    )
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    bookmark = current_user.bookmarks.find_by(post: @post)
+    bookmark.destroy
+    
+    render turbo_stream: turbo_stream.replace(
+      "bookmark_button_#{@post.id}",
+      partial: "bookmarks/bookmark",
+      locals: { post: @post }
+    )
   end
 end
